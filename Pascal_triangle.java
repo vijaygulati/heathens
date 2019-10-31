fa668558ac263247bf11ef3d8928ea5d07749821
vijaygulati <56892492+vijaygulati@users.noreply.github.com>
2019-10-31 23:13:04 +0530

    Update Pascal_triangle.java

--- Pascal_triangle.java
+++ Pascal_triangle.java
@@ -8,21 +8,21 @@ public class Pascal_triangle {
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Enter Number of Rows : ");
-        r = scan.nextInt();
+        r = scan.nextInt();     // input the no. of rows in the pattern of the pascal trianlge
 
-        for(i=0;i<r;i++)
+        for(i=0;i<r;i++)        // loop for the rows
         {
-            for(k=r; k>i; k--)
+            for(k=r; k>i; k--)      // loop for the spaces
             {
                 System.out.print(" ");
             }
             number = 1;
-            for(j=0;j<=i;j++)
+            for(j=0;j<=i;j++)       // loop for the columns
             {
                 System.out.print(number+ " ");
                 number = number * (i - j) / (j + 1);
             }
-            System.out.println();
+            System.out.println();   // jump to the next line
         }
     }
 }
